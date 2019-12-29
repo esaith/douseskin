2dd732b4ffebf8a6fefc28ce9cba1cf4f0b37f0c
esaith <hounds2003@hotmail.com>
2019-12-29 14:33:53 -0800

    Removed networking event pages. Removed .css files as they are generated

--- controller/networkEventController.js
+++
@@ -1,7 +0,0 @@
-angular.module('douse')
-    .controller('networkingEventController', ['$scope', '$window',
-        function networkEventController($scope, $window) {
-            $scope.goTo = function(route) {
-                $window.location.href = "#!/" + route;
-            }
-        }]);
